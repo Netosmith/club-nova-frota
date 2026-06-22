5973c375cb569fafa52765858d2d05ddc09e857e
Netosmith <luziano.transportes@gmail.com>
2026-06-22 16:21:38 -0300

    app: adiciona cores oficiais

---
+++ app_motorista/lib/core/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color azulPrincipal = Color(0xFF003C8F);
+  static const Color azulEscuro = Color(0xFF002A64);
+  static const Color verdePrincipal = Color(0xFF00B140);
+  static const Color verdeClaro = Color(0xFF4CD964);
+  static const Color branco = Color(0xFFFFFFFF);
+  static const Color cinzaClaro = Color(0xFFF4F6F8);
+  static const Color cinzaTexto = Color(0xFF6B7280);
+  static const Color textoPrincipal = Color(0xFF111827);
+}
