8c73f2d87b59516a704a08f823433b7621a940d9
Netosmith <luziano.transportes@gmail.com>
2026-06-22 18:02:47 -0300

    app: adiciona serviço base do Firestore

---
+++ app_motorista/lib/core/services/firestore_service.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  FirestoreService({FirebaseFirestore? firestore})
+      : _firestore = firestore ?? FirebaseFirestore.instance;
+
+  final FirebaseFirestore _firestore;
+
+  CollectionReference<Map<String, dynamic>> collection(String path) {
+    return _firestore.collection(path);
+  }
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getDocument({
+    required String collectionPath,
+    required String documentId,
+  }) {
+    return collection(collectionPath).doc(documentId).get();
+  }
+
+  Future<void> setDocument({
+    required String collectionPath,
+    required String documentId,
+    required Map<String, dynamic> data,
+  }) {
+    return collection(collectionPath).doc(documentId).set(data);
+  }
+
+  Future<void> updateDocument({
+    required String collectionPath,
+    required String documentId,
+    required Map<String, dynamic> data,
+  }) {
+    return collection(collectionPath).doc(documentId).update(data);
+  }
+
+  Future<void> addDocument({
+    required String collectionPath,
+    required Map<String, dynamic> data,
+  }) {
+    return collection(collectionPath).add(data);
+  }
+}
