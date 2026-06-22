ac4c06dc21d704f62e279227edc5c2cf53096734
Netosmith <luziano.transportes@gmail.com>
2026-06-22 17:39:51 -0300

    app: adiciona rota principal com menu inferior

--- app_motorista/lib/core/routes/app_routes.dart
+++ app_motorista/lib/core/routes/app_routes.dart
@@ -6,9 +6,11 @@ import '../../features/fretes/fretes_screen.dart';
 import '../../features/home/home_screen.dart';
 import '../../features/ordens/ordens_screen.dart';
 import '../../features/perfil/perfil_screen.dart';
+import '../../shared/widgets/main_navigation.dart';
 
 class AppRoutes {
   static const String login = '/login';
+  static const String main = '/main';
   static const String home = '/home';
   static const String fretes = '/fretes';
   static const String ordens = '/ordens';
@@ -18,6 +20,7 @@ class AppRoutes {
   static Map<String, WidgetBuilder> get routes {
     return {
       login: (_) => const LoginScreen(),
+      main: (_) => const MainNavigation(),
       home: (_) => const HomeScreen(),
       fretes: (_) => const FretesScreen(),
       ordens: (_) => const OrdensScreen(),
