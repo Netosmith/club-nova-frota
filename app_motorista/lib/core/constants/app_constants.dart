42028ab1665554f32547c184deafb28a6c79548a
Netosmith <luziano.transportes@gmail.com>
2026-06-22 18:29:26 -0300

    app: adiciona constantes gerais

---
+++ app_motorista/lib/core/constants/app_constants.dart
@@ -0,0 +1,12 @@
+class AppConstants {
+  static const String appName = 'Club Nova Frota';
+  static const String portalName = 'Portal Frete';
+  static const String slogan = 'A comunidade do motorista';
+
+  static const String pacoteAndroid = 'br.com.portalfrete.clubnovafrota';
+
+  static const int pontosOrdemFinalizada = 50;
+  static const int pontosComprovanteAprovado = 20;
+  static const int pontosViagemSemPendencia = 30;
+  static const int pontosIndicacao = 500;
+}
