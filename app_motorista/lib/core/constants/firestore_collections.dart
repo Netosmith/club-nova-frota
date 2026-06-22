e4c96d62df35ccd1784689b83d923856dfb0f815
Netosmith <luziano.transportes@gmail.com>
2026-06-22 18:29:15 -0300

    app: adiciona constantes das coleções Firestore

---
+++ app_motorista/lib/core/constants/firestore_collections.dart
@@ -0,0 +1,15 @@
+class FirestoreCollections {
+  static const String usuarios = 'usuarios';
+  static const String motoristas = 'motoristas';
+  static const String fretes = 'fretes';
+  static const String ordens = 'ordens';
+
+  static const String pontos = 'pontos';
+  static const String medalhas = 'medalhas';
+  static const String posts = 'posts';
+  static const String comentarios = 'comentarios';
+  static const String curtidas = 'curtidas';
+  static const String beneficios = 'beneficios';
+  static const String resgates = 'resgates';
+  static const String notificacoes = 'notificacoes';
+}
