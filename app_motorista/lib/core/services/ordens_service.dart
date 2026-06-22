01a7116eed23700f5b83b16d3b8161f2123e1281
Netosmith <luziano.transportes@gmail.com>
2026-06-22 18:03:23 -0300

    app: adiciona serviço de ordens

---
+++ app_motorista/lib/core/services/ordens_service.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../shared/models/ordem_model.dart';
+import 'firestore_service.dart';
+
+class OrdensService {
+  OrdensService({FirestoreService? firestoreService})
+      : _firestoreService = firestoreService ?? FirestoreService();
+
+  final FirestoreService _firestoreService;
+
+  Stream<List<OrdemModel>> listarOrdensDoMotorista(String motoristaId) {
+    return _firestoreService
+        .collection('ordens')
+        .where('motoristaId', isEqualTo: motoristaId)
+        .snapshots()
+        .map(_mapOrdens);
+  }
+
+  Future<void> solicitarOrdem({
+    required String motoristaId,
+    required String freteId,
+  }) {
+    return _firestoreService.addDocument(
+      collectionPath: 'ordens',
+      data: {
+        'motoristaId': motoristaId,
+        'freteId': freteId,
+        'status': 'solicitada',
+        'comprovanteUrl': '',
+        'observacoes': '',
+        'solicitadaEm': FieldValue.serverTimestamp(),
+        'criadoEm': FieldValue.serverTimestamp(),
+        'atualizadoEm': FieldValue.serverTimestamp(),
+      },
+    );
+  }
+
+  List<OrdemModel> _mapOrdens(QuerySnapshot<Map<String, dynamic>> snapshot) {
+    return snapshot.docs
+        .map((doc) => OrdemModel.fromMap(doc.id, doc.data()))
+        .toList();
+  }
+}
