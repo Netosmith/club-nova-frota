a38e98621221cde1c4591ac667aa226b22d8429d
Netosmith <luziano.transportes@gmail.com>
2026-06-23 10:13:57 -0300

    painel admin: registra rotas no app

--- painel_admin/lib/app.dart
+++ painel_admin/lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'core/routes/admin_routes.dart';
 import 'core/theme/admin_theme.dart';
 import 'features/dashboard/dashboard_screen.dart';
 
@@ -12,6 +13,8 @@ class PainelAdminApp extends StatelessWidget {
       title: 'Painel Admin - Club Nova Frota',
       debugShowCheckedModeBanner: false,
       theme: AdminTheme.light,
+      routes: AdminRoutes.routes,
+      initialRoute: AdminRoutes.dashboard,
       home: const DashboardScreen(),
     );
   }
