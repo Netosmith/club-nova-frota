ed672fc18e4867d0988254261bae13dfed6a72f9
Netosmith <luziano.transportes@gmail.com>
2026-06-23 10:11:33 -0300

    painel admin: adiciona rotas iniciais

---
+++ painel_admin/lib/core/routes/admin_routes.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../features/comprovantes/comprovantes_screen.dart';
+import '../../features/dashboard/dashboard_screen.dart';
+import '../../features/fretes/fretes_screen.dart';
+import '../../features/motoristas/motoristas_screen.dart';
+import '../../features/ordens/ordens_screen.dart';
+
+class AdminRoutes {
+  static const String dashboard = '/dashboard';
+  static const String motoristas = '/motoristas';
+  static const String fretes = '/fretes';
+  static const String ordens = '/ordens';
+  static const String comprovantes = '/comprovantes';
+
+  static Map<String, WidgetBuilder> get routes {
+    return {
+      dashboard: (_) => const DashboardScreen(),
+      motoristas: (_) => const MotoristasScreen(),
+      fretes: (_) => const FretesScreen(),
+      ordens: (_) => const OrdensScreen(),
+      comprovantes: (_) => const ComprovantesScreen(),
+    };
+  }
+}
