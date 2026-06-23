fda0f2ae414cc527c4d6c80351e0d7b94ffe9db1
Netosmith <luziano.transportes@gmail.com>
2026-06-23 10:00:57 -0300

    painel admin: adiciona cores oficiais

---
+++ painel_admin/lib/core/theme/admin_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AdminColors {
+  static const Color azulPrincipal = Color(0xFF003C8F);
+  static const Color azulEscuro = Color(0xFF002A64);
+  static const Color verdePrincipal = Color(0xFF00B140);
+  static const Color branco = Color(0xFFFFFFFF);
+  static const Color cinzaFundo = Color(0xFFF4F6F8);
+  static const Color cinzaTexto = Color(0xFF6B7280);
+  static const Color textoPrincipal = Color(0xFF111827);
+}
