e6d6e54568d6b10cbf3e07b57eb63eb84a822062
Netosmith <luziano.transportes@gmail.com>
2026-06-23 10:12:36 -0300

    painel admin: adiciona layout padrão

---
+++ painel_admin/lib/shared/widgets/admin_layout.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'admin_sidebar.dart';
+
+class AdminLayout extends StatelessWidget {
+  const AdminLayout({
+    super.key,
+    required this.title,
+    required this.child,
+  });
+
+  final String title;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          const AdminSidebar(),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                AppBar(title: Text(title)),
+                Expanded(child: child),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
