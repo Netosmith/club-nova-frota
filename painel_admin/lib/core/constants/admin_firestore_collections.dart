57fc9a0005f3fe286bdf9d62c65a858de9cd7c24
Netosmith <luziano.transportes@gmail.com>
2026-06-23 11:09:45 -0300

    painel admin: adiciona constantes Firestore

---
+++ painel_admin/lib/core/constants/admin_firestore_collections.dart
@@ -0,0 +1,14 @@
+class AdminFirestoreCollections {
+  static const String usuarios = 'usuarios';
+  static const String motoristas = 'motoristas';
+  static const String fretes = 'fretes';
+  static const String ordens = 'ordens';
+  static const String comprovantes = 'comprovantes';
+  static const String pontos = 'pontos';
+  static const String ranking = 'ranking';
+  static const String beneficios = 'beneficios';
+  static const String resgates = 'resgates';
+  static const String notificacoes = 'notificacoes';
+  static const String logs = 'logs';
+  static const String configuracoes = 'configuracoes';
+}
